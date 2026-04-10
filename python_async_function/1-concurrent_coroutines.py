5398b882e873f3e9610fb7142a3ab9d6facc2c44
aourzik <a.ourzik@gmail.com>
2026-04-10 10:47:15 +0200

    feat: Implement asynchronous wait_n function using concurrent tasks

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""async routine wait_n."""
+"""Module that contains an async routine wait_n."""
 
 import asyncio
 
@@ -7,8 +7,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list:
-    """Spawn wait_random n times and return sorted list of delays."""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    """Spawn wait_random n times and return results in ascending order."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
 
     results = []
     for task in asyncio.as_completed(tasks):
