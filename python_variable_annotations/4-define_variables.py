09f2156353e62ef12b1f21e750d97ceb78ae5302
aourzik <a.ourzik@gmail.com>
2026-04-10 09:58:49 +0200

    feat: Define and annotate the following variables with the specified values:

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module contains a floor function that takes
+a float n as argument and returns the floor of the float."""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a few variables types."""
+    return math.floor(n)
+
+
+a: float = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
