1d48a820c0ae5758d71c4f87543ae4af519978fb
aourzik <a.ourzik@gmail.com>
2026-04-10 11:56:58 +0200

    feat: Import async_generator from the previous task and then write a coroutine called async_comprehension that takes no arguments.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_comprehension() -> Generator[float, None, None]: # type: ignore
     """Asynchronously yield 10 random numbers between 0 and 10.
 
     Waits 1 second between each yield.
