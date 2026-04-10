408d3512d5c53c3c7df1f3eef183343cd15b38ac
aourzik <a.ourzik@gmail.com>
2026-04-10 11:52:02 +0200

    feat: Write a coroutine called async_generator that takes no arguments.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,9 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+
+async def async_generator():
     """Asynchronously yield 10 random numbers between 0 and 10.
 
     Waits 1 second between each yield.
