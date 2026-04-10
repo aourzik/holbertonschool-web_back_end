2ae6d68b85d7c0c1d9256f165b628142c33504f6
aourzik <a.ourzik@gmail.com>
2026-04-10 10:23:44 +0200

    feat: Module that provides a function to compute element lengths.

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Module that provides a function to compute element lengths."""
 
-from typing import List, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[str, int]]:
     """Return a list of tuples containing each string and its length.
 
     Args:
-        lst (List[str]): A list of strings.
+        lst (Iterable[Sequence]): An iterable of sequences (e.g., strings).
 
     Returns:
         List[Tuple[str, int]]: A list of tuples where each tuple contains
