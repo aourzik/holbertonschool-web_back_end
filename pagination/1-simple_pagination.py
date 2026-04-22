1fc106721f2352805829e047030e23f1cf9d18b3
aourzik <a.ourzik@gmail.com>
2026-04-22 15:45:09 +0200

    fix: modify to be pycodstyle friendly

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,41 +1,37 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Simple pagination module"""
+
+
 import csv
-import math
-from typing import List
-
-"""Simple pagination"""
-
-def index_range(page, page_size):
-    """Returns a tuple of size two containing a start index and an end index"""
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+from typing import List, Tuple
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a dataset of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return a page of the dataset based on pagination parameters.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-
-        if start >= len(data):
-            return []
-
-        return data[start:end]
+        start_index, end_index = index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
