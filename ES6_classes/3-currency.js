dcf159488a0de3e2f87853f8fefb447bc9f618e3
aourzik <a.ourzik@gmail.com>
2026-04-29 10:25:05 +0200

    feat: Implement a class named Currency

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,35 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // code
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  // name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // method
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
